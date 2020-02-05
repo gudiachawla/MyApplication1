3152d04601f905326e03a78facea0b43b72e8f0d
student <uic.18mca8039@gmail.com>
2020-02-05 15:59:37 +0530

    First Commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+     ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.img);
+        Intent intent = getIntent();
+        Uri ab = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+        imageView.setImageURI(ab);
+    }
+
+        }
+
